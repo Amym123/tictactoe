47ff5c2d11a21d01585d041ac17c89863a6db1da
Amy Mao <amy_m123@icloud.com>
2018-12-18 23:12:35 -0800

    basic building blocks done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Square extends React.Component {
+  // constructor to initialize state
+  // in javascript classes, always call super when defining "constructor" of a subclass
+  // all react component classes that have a "constructor" sthould start it with a "super(props)" call.
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    };
+    // React components can use state to remember things. private to defined component.
+    // store current value of Square in "this.state", change it when Square is clicked.
+  }
+  render() {
+    return (
+      // <button className="square" onClick={function() {alert('click');}}>
+      // arrow function syntax for event handlers
+      // onClick={() => alert('click')} : alert when a square is clicked
+        // <button
+        //   className="square"
+        //   onClick={() => alert('click')}>
+        //   {this.props.value}
+        // </button>
+        // passes a prop from a parent Board component to a child Square component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      // displays the current state's value when clicked.
+      // onClick={() => this.setState({value: 'X'})}>: tells react to re-render square whenever its button
+      // is clicked. after update, square's this.state.value = 'X'.
+
+      // ** when setState is called in a component, react automatically updates the child components inside of it too
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i}/>;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
